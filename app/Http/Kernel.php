5c98f4a60e9c8312bc009ba0d7057ac63c175f1a
ResalR92 <resalramdahadi92@gmail.com>
2017-01-10 17:00:35 +0700

    membuat middleware redirect if not -> admin,operator,peserta

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,5 +52,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin' => \App\Http\Middleware\RedirectIfNotAdmin::class,
+        'operator' => \App\Http\Middleware\RedirectIfNotOperator::class,
+        'peserta' => \App\Http\Middleware\RedirectIfNotOperator::class,
     ];
 }
