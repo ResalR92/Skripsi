7fd25e1ab8e34a2f9a36488ada099e308ec2f3a3
ResalR92 <resalramdahadi92@gmail.com>
2017-01-12 09:31:25 +0700

    membuat list_jurusan -> provider

---
+++ app/Providers/FormPesertaServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Jurusan;
+
+class FormPesertaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer('admin.peserta._form',function($view){
+            $view->with('list_jurusan',Jurusan::all()->pluck('nama','id'));
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
