4a778af00fe4d5dc90e7553509a472235308d23f
ResalR92 <resalramdahadi92@gmail.com>
2017-01-15 06:05:46 +0700

    membuat UsersSeeder baru->laratrust->role

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 use App\User;
 
 class UsersSeeder extends Seeder
@@ -12,19 +13,40 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+        // Membuat role admin
+        $adminRole = new Role();
+        $adminRole->name = "admin";
+        $adminRole->display_name = "Admin";
+        $adminRole->save();
+
+        // Membuat role operator
+        $operatorRole = new Role();
+        $operatorRole->name = "operator";
+        $operatorRole->display_name = "Operator";
+        $operatorRole->save();
+
+        // Membuat role peserta
+        $pesertaRole = new Role();
+        $pesertaRole->name = "peserta";
+        $pesertaRole->display_name = "Peserta";
+        $pesertaRole->save();
+
+
         //membuat sample admin
         $admin = new User();
         $admin->name ='Administrator';
         $admin->email = 'admin@panjatek.com';
         $admin->password = bcrypt('rahasia');
         $admin->save();
+        $admin->attachRole($adminRole);
 
         //membuat sample operator
         $operator = new User();
         $operator->name ='Operator';
         $operator->email = 'operator@panjatek.com';
         $operator->password = bcrypt('rahasia');
-        $operator->save();  
+        $operator->save(); 
+        $operator->attachRole($operatorRole); 
 
         //membuat sample peserta
         $peserta = new User();
@@ -32,6 +54,7 @@ class UsersSeeder extends Seeder
         $peserta->email = 'taylor@mail.com';
         $peserta->password = bcrypt('rahasia');
         $peserta->save();
+        $peserta->attachRole($pesertaRole);
 
         //membuat sample peserta
         $paulus = new User();
@@ -39,6 +62,7 @@ class UsersSeeder extends Seeder
         $paulus->email = 'paulus@mail.com';
         $paulus->password = bcrypt('rahasia');
         $paulus->save();
+        $paulus->attachRole($pesertaRole);
 
         //membuat sample peserta
         $yakobus = new User();
@@ -46,5 +70,6 @@ class UsersSeeder extends Seeder
         $yakobus->email = 'yakobus@mail.com';
         $yakobus->password = bcrypt('rahasia');
         $yakobus->save();
+        $yakobus->attachRole($pesertaRole);
     }
 }
