c30a5270f8d89214bb5a8daa3be143d230256a3b
ResalR92 <resalramdahadi92@gmail.com>
2017-01-15 06:22:50 +0700

    membuat register -> otomatis -> role -> peserta dan fix-> seed->kontak, pengumuman

--- database/seeds/KontakSeeder.php
+++ database/seeds/KontakSeeder.php
@@ -12,9 +12,9 @@ class KontakSeeder extends Seeder
     public function run()
     {
         $kontak = [
-        	['nama'=>'Resal Ramdahadi','email'=>'resalramdahadi92@gmail.com','judul'=>'Lupa Password','isi'=>'Admin, tolong kirim password akun saya ke email, trims.','dibalas'=>true],
-        	['nama'=>'M. Ghufron A.','email'=>'gufron@mail.com','judul'=>'Info Terbaru Kelulusan','isi'=>'Admin, apakah saya sudah diterima di SMK Panjatek, trims.','dibalas'=>false],
-        	['nama'=>'Franco Escobar','email'=>'franco@mail.com','judul'=>'Info Terbaru Kelulusan','isi'=>'Admin, apakah saya sudah diterima di SMK Panjatek, trims.','dibalas'=>true],
+        	['nama'=>'Resal Ramdahadi','email'=>'resalramdahadi92@gmail.com','judul'=>'Lupa Password','isi'=>'Admin, tolong kirim password akun saya ke email, trims.','dibalas'=>true,'created_at'=>'2017-01-07 00:00:00','updated_at'=>'2017-01-07 00:00:00'],
+        	['nama'=>'M. Ghufron A.','email'=>'gufron@mail.com','judul'=>'Info Terbaru Kelulusan','isi'=>'Admin, apakah saya sudah diterima di SMK Panjatek, trims.','dibalas'=>false,'created_at'=>'2017-01-08 00:00:00','updated_at'=>'2017-01-08 00:00:00'],
+        	['nama'=>'Franco Escobar','email'=>'franco@mail.com','judul'=>'Info Terbaru Kelulusan','isi'=>'Admin, apakah saya sudah diterima di SMK Panjatek, trims.','dibalas'=>true,'created_at'=>'2017-01-09 00:00:00','updated_at'=>'2017-01-09 00:00:00'],
         ];
 
         //Masukkan data ke database
--- database/seeds/PengumumanSeeder.php
+++ database/seeds/PengumumanSeeder.php
@@ -12,8 +12,8 @@ class PengumumanSeeder extends Seeder
     public function run()
     {
         $pengumuman = [
-        	['judul'=>'Cara Update Data #1','isi'=>'Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini.'],
-        	['judul'=>'Cara Update Data #2','isi'=>'Bagi anda yang belum tahu cara #2 mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini.'],
+        	['judul'=>'Cara Update Data #1','isi'=>'Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini.','created_at'=>'2017-01-07 00:00:00','updated_at'=>'2017-01-07 00:00:00'],
+        	['judul'=>'Cara Update Data #2','isi'=>'Bagi anda yang belum tahu cara #2 mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini. Bagi anda yang belum tahu cara mengupdate data, maka ikuti langkah berikut ini.','created_at'=>'2017-01-08 00:00:00','updated_at'=>'2017-01-08 00:00:00'],
         	
         ];
 
