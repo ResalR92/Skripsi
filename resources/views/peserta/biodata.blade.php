b6f9796a0fabbaab520e2272ec484d27165f0e0a
ResalR92 <resalramdahadi92@gmail.com>
2017-01-17 02:35:52 +0700

    menyesuaikan status->warning, success -> tombol -> edit,pdf

--- resources/views/peserta/biodata.blade.php
+++ resources/views/peserta/biodata.blade.php
@@ -8,43 +8,53 @@
 		    </div>
 		</div>
 		@if(isset($peserta))
-			
 			@foreach($peserta as $biodata)
-				<div class="col-md-2 col-md-offset-1">
-					@if(isset($biodata->foto))
-						{!! Html::image(asset('fotoupload/'.$biodata->foto),null,['class'=>'img-rounded img-responsive','width'=>'200px']) !!}
-					@else
-						@if($biodata->jenis_kelamin == 'L')
-							<img src="{{ asset('fotoupload/dummymale.jpg') }}">
+				<div class="row">
+					<div class="col-md-2 col-md-offset-1">
+						@if(isset($biodata->foto))
+							{!! Html::image(asset('fotoupload/'.$biodata->foto),null,['class'=>'img-rounded img-responsive','width'=>'200px']) !!}
 						@else
-							<img src="{{ asset('fotoupload/dummyfemale.jpg') }}">
+							@if($biodata->jenis_kelamin == 'L')
+								<img src="{{ asset('fotoupload/dummymale.jpg') }}">
+							@else
+								<img src="{{ asset('fotoupload/dummyfemale.jpg') }}">
+							@endif
 						@endif
-					@endif
-					
-				</div>
-				<div class="col-md-6">
-					<table class="table table-striped">
-						<tr>
-							<th width="150">No. Pendaftaran</th>
-							<td>{{ $biodata->id }}</td>
-						</tr>
-						<tr>
-							<th>Nama Lengkap</th>
-							<td>{{ $biodata->nama }}</td>
-						</tr>
-						<tr>
-							<th>Program Keahlian</th>
-							<td>{{ $biodata->jurusan->nama }}</td>
-						</tr>
-						<tr>
-							<th>Sekolah Asal</th>
-							<td>{{ $biodata->sekolah->nama }}</td>
-						</tr>
-						<tr>
-							<th>Status</th>
-							<td><h1><span class="label label-{{ $biodata->status->label }}">{{ $biodata->status->nama }}</span></h1></td>
-						</tr>
-					</table>
+						<hr>
+						@if($biodata->status->label == 'warning')
+							{{ link_to('biodata/'.$biodata->id.'/edit','Edit',['class'=>'btn btn-warning']) }}
+						@elseif($biodata->status->label == 'success')
+							<a href="#" class="btn btn-success">Cetak PDF</a>
+						@endif
+					</div>
+					<div class="col-md-6">
+						<table class="table table-striped">
+							<tr>
+								<th width="150">No. Pendaftaran</th>
+								<td>{{ $biodata->id }}</td>
+							</tr>
+							<tr>
+								<th>Nama Lengkap</th>
+								<td>{{ $biodata->nama }}</td>
+							</tr>
+							<tr>
+								<th>Program Keahlian</th>
+								<td>{{ $biodata->jurusan->nama }}</td>
+							</tr>
+							<tr>
+								<th>Sekolah Asal</th>
+								<td>{{ $biodata->sekolah->nama }}</td>
+							</tr>
+							<tr>
+								<th>Status</th>
+								<td><h1><span class="label label-{{ $biodata->status->label }}">{{ $biodata->status->nama }}</span></h1></td>
+							</tr>
+							<tr>
+								<th></th>
+								<td>{{ $biodata->status->pesan }}</td>
+							</tr>
+						</table>
+					</div>
 				</div>
 			@endforeach
 		@else
