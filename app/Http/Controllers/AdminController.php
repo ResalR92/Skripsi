0861f14bb8c1b7eddfa4c148f58cd6ed494ad107
ResalR92 <resalramdahadi92@gmail.com>
2017-01-19 21:00:15 +0700

    membuat dashboard -> chart.js->awal

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,10 +12,24 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $peserta = Peserta::all()->count();
-        $jurusan = Jurusan::all()->count();
-        $kontak = Kontak::all()->count();
-		return view('dashboard.admin',compact('peserta','jurusan','kontak'));
+        $peserta = Peserta::all();
+        $jurusan = Jurusan::all();
+        $kontak = Kontak::all();
+
+        $jml_peserta = $peserta->count();
+        $jml_jurusan = $jurusan->count();
+        $jml_kontak = $kontak->count();
+
+        $tb_jurusan = [];
+        $tb_peserta = [];
+
+        foreach($jurusan as $jurusan){
+            array_push($tb_jurusan, $jurusan->nama);
+            array_push($tb_peserta, $jurusan->peserta->count());
+        }
+
+
+		return view('dashboard.admin',compact('jml_peserta','jml_jurusan','jml_kontak','tb_jurusan','tb_peserta'));
     }
 
     public function myadmin()
--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -12,6 +12,7 @@
 		        </ol>
 		    </div>
 		</div>
+		
 		<div class="row">
 		    <div class="col-lg-4 col-md-6">
 		        <div class="panel panel-primary">
@@ -21,7 +22,7 @@
 		                        <i class="fa fa-users fa-5x"></i> 
 		                    </div>
 		                    <div class="col-xs-9 text-right">
-		                        <div class="huge">{{ $peserta }}</div>
+		                        <div class="huge">{{ $jml_peserta }}</div>
 		                        <div>Peserta</div>
 		                    </div>
 		                </div>
@@ -44,7 +45,7 @@
 		                        <i class="fa fa-graduation-cap fa-5x"></i>
 		                    </div>
 		                    <div class="col-xs-9 text-right">
-		                        <div class="huge">{{ $jurusan }}</div>
+		                        <div class="huge">{{ $jml_jurusan }}</div>
 		                        <div>Jurusan</div>
 		                    </div>
 		                </div>
@@ -67,7 +68,7 @@
 		                        <i class="fa fa-phone-square fa-5x"></i>
 		                    </div>
 		                    <div class="col-xs-9 text-right">
-		                        <div class="huge">3</div>
+		                        <div class="huge">{{ $jml_kontak }}</div>
 		                        <div>Kontak</div>
 		                    </div>
 		                </div>
@@ -82,5 +83,49 @@
 		        </div>
 		    </div>
 		</div>
+		<div class="row">
+			<div class="col-md-9">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<h4>Statistik Peserta</h4>
+						<canvas id="chartPenulis" width="400" height="150"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 @stop
+
+@section('scripts')
+	<script src="{{ asset('js/Chart.min.js') }}"></script>
+	<script type="text/javascript">
+		var data = {
+			labels: {!! json_encode($tb_jurusan) !!},
+			datasets: [{
+				label:'Jumlah Peserta',
+				data: {!! json_encode($tb_peserta) !!},
+				backgroundColor: "rgba(151,187,205,0.5)",
+				borderColor: "rgba(151,187,205,0.8)",
+			}]
+		};
+
+		var options = {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true,
+						stepSize: 1
+					}
+				}]
+			}
+		};
+
+		var ctx = document.getElementById("chartPenulis").getContext("2d");
+
+		var authorChart = new Chart(ctx, {
+			type: 'bar',
+			data: data,
+			options: options
+		});
+	</script>
+@endsection
